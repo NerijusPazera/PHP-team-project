e230b80f3267e2c7ca65795f182a905513be0ad2
TitasLukosius <titasgitara@gmail.com>
2020-03-13 10:39:58 +0200

    navigation

---
+++ navigation.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/navigation.css">
+    </head>
+<header>
+    <div class ='logo'>
+        <img src="assets/images/A%20team.png">
+    </div>
+    <div class ='navigation'>
+        <nav>
+            <ul>
+                <li><a href ="index.php">Home</a></li>
+                <li><a href ="advance.php">Advance</a></li>
+                <li><a href ="fight.php">Fight-club</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+</html>
